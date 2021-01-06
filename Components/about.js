9cff2ca27c41d2de3ef4696d6a68859fbb379efb
Raymond0T <36683390+Raymond0T@users.noreply.github.com>
2021-01-06 13:36:19 -0800

    Add files via upload

--- Components/about.js
+++ Components/about.js
@@ -7,13 +7,13 @@ function About(){
         <div id="about-container"> 
             <div className="about-section">
                 <h1>
-                About
+                    About
                 </h1>
                 <p>
                     Hi, my name is Raymond Trau. I recenetly graduated from Univeristy of California, Merced during Fall 2020 with a B.S in Computer Science and Engineering.
-                        
-                        <br/><br/><br/>
-                        Feel free to connect with me on LinkedIn! 
+                            
+                    <br/><br/><br/>
+                    Feel free to connect with me on LinkedIn! 
                 </p>
             </div>
         </div>
